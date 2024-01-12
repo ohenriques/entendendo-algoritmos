6a13f12e4d8b20eb3de3316d47fa94d889a075a2
Paulo Souza <rick.souza97@gmail.com>
2024-01-12 14:41:50 -0300

    finalizando capitulo 1

--- Java/entendendo-algoritmos/src/introducaoAAlgoritmos/BuscaBinaria.java
+++ Java/entendendo-algoritmos/src/introducaoAAlgoritmos/BuscaBinaria.java
@@ -3,15 +3,31 @@ package introducaoAAlgoritmos;
 public class BuscaBinaria {
 
     public static void main(String[] args) {
-        int[] minhaLista = { 3, 5, 6, 7, 8, 19, 40 };
+        int[] minhaLista = {3, 5, 6, 7, 8, 19, 40, 42};
 
-        BuscaBinaria pb = new BuscaBinaria();
+        BuscaBinaria bb = new BuscaBinaria();
 
-        pb.pesquisaBinari(minhaLista, 3);
+        System.out.println(bb.buscaBunaria(minhaLista, 42));
+        System.out.println(bb.buscaBunaria(minhaLista, 19));
+        System.out.println(bb.buscaBunaria(minhaLista, 40));
+
     }
 
-    public void pesquisaBinari(int[] minhaLista, int item) {
-        int baixo = 0;
-        int alto = minhaLista.length;
+    public Integer buscaBunaria(int[] lista, int item) {
+        int indiceBaixo = 0;
+        int indiceAlto = lista.length;
+        int etapa = 0;
+
+        while (indiceBaixo <= indiceAlto) {
+            int indiceMeio = (indiceBaixo + indiceAlto) / 2;
+            int chute = lista[indiceMeio];
+            if (chute == item) {
+                return indiceMeio;
+            }
+            if (chute > item) {
+                indiceAlto = indiceMeio - 1;
+            } else indiceBaixo = indiceMeio + 1;
+        }
+        return null;
     }
 }
